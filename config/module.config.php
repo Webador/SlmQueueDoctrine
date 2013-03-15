f10db0248998ba025bd2ae3f50373037f80e4fb4
Bas Kamer <bas@bushbaby.nl>
2013-03-15 20:19:42 +0100

    more explicit driver key

--- config/module.config.php
+++ config/module.config.php
@@ -52,13 +52,13 @@ return array(
 
     'doctrine' => array(
         'driver' => array(
-            'queuejob_entity' => array(
+            'slm_queue_driver' => array(
                 'class' => 'Doctrine\ORM\Mapping\Driver\XmlDriver',
                 'paths' => __DIR__ . '/../data/doctrine'
             ),
             'orm_default' => array(
                 'drivers' => array(
-                    'SlmQueueDoctrine\Entity'  => 'queuejob_entity'
+                    'SlmQueueDoctrine\Entity'  => 'slm_queue_driver'
                 )
             )
         )
