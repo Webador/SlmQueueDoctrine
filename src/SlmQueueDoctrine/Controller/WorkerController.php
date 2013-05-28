1c968d65b084518dff2a847a10d36192976b7732
Bas Kamer <bas@bushbaby.nl>
2013-05-28 11:58:28 +0200

    refactor to use a controller factory

--- src/SlmQueueDoctrine/Controller/WorkerController.php
+++ src/SlmQueueDoctrine/Controller/WorkerController.php
@@ -5,8 +5,6 @@ namespace SlmQueueDoctrine\Controller;
 use Exception;
 use SlmQueueDoctrine\Queue\Table;
 use SlmQueue\Controller\AbstractWorkerController;
-use SlmQueue\Controller\Exception\WorkerProcessException;
-use SlmQueue\Exception\SlmQueueExceptionInterface;
 
 /**
  * Worker controller
@@ -15,42 +13,16 @@ class WorkerController extends AbstractWorkerController
 {
 
     /**
-     * {@inheritDoc}
-     */
-    protected function getWorker() {
-        /** @var $worker \SlmQueueDoctrine\Worker\Worker */
-        $worker    = $this->serviceLocator->get('SlmQueueDoctrine\Worker\Worker');
-
-        return $worker;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function getOptions() {
-
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    protected function getQueueName() {
-        $queueName = $this->params('queueName');
-
-        return $queueName;
-    }
-
-    /**
      * Recover long running jobs
      *
      * @return string
      */
     public function recoverAction()
     {
-        $queueName     = $this->getQueueName();
+        $queueName     = $this->params('queue');
         $executionTime = $this->params('executionTime', 0);
 
-        /** @var $queueManager \SlmQueue\Queue\QueuepluginManager */
+        /** @var $queueManager \SlmQueue\Queue\QueuePluginManager */
         $queueManager = $this->getServiceLocator()->get('SlmQueue\Queue\QueuePluginManager');
         $queue        = $queueManager->get($queueName);
 
@@ -60,8 +32,8 @@ class WorkerController extends AbstractWorkerController
 
         try {
             $count = $queue->recover($executionTime);
-        } catch(\Exception $exception) {
-            throw new \Exception("An error occurred", null, $exception);
+        } catch(Exception $exception) {
+            throw new Exception("An error occurred", null, $exception);
         }
 
         return sprintf(
