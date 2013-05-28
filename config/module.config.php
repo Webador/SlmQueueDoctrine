0c970460181f55f0bda069888d426c58d0cc316b
Bas Kamer <bas@bushbaby.nl>
2013-05-28 11:58:28 +0200

    refactor to use a controller factory

--- config/module.config.php
+++ config/module.config.php
@@ -4,7 +4,8 @@ return array(
     'service_manager' => array(
         'factories' => array(
             'SlmQueueDoctrine\Options\DoctrineOptions' => 'SlmQueueDoctrine\Factory\DoctrineOptionsFactory',
-            'SlmQueueDoctrine\Worker\Worker'           => 'SlmQueueDoctrine\Factory\WorkerFactory'
+            'SlmQueueDoctrine\Worker\Worker'           => 'SlmQueueDoctrine\Factory\WorkerFactory',
+            'SlmQueueDoctrine\Controller\Worker'       => 'SlmQueueDoctrine\Factory\WorkerControllerFactory'
         )
     ),
 
@@ -14,7 +15,7 @@ return array(
                 'slm-queue-doctrine-worker' => array(
                     'type'    => 'Simple',
                     'options' => array(
-                        'route'    => 'queue doctrine <queueName> [--timeout=] --start',
+                        'route'    => 'queue doctrine <queue> [--timeout=] --start',
                         'defaults' => array(
                             'controller' => 'SlmQueueDoctrine\Controller\Worker',
                             'action'     => 'process'
@@ -24,7 +25,7 @@ return array(
                 'slm-queue-doctrine-recover' => array(
                     'type'    => 'Simple',
                     'options' => array(
-                        'route'    => 'queue doctrine <queueName> --recover [--executionTime=]',
+                        'route'    => 'queue doctrine <queue> --recover [--executionTime=]',
                         'defaults' => array(
                             'controller' => 'SlmQueueDoctrine\Controller\Worker',
                             'action'     => 'recover'
@@ -33,12 +34,6 @@ return array(
                 ),
             ),
         ),
-    ),
-
-    'controllers' => array(
-        'invokables' => array(
-            'SlmQueueDoctrine\Controller\Worker' => 'SlmQueueDoctrine\Controller\WorkerController'
-        )
     ),
 
     'slm_queue' => array(
