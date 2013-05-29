0c1e696f376b3b6646c4335b3a6b5e916507ab93
Bas Kamer <bas@bushbaby.nl>
2013-05-29 22:45:13 +0200

    cs

--- src/SlmQueueDoctrine/Factory/DoctrineQueueFactory.php
+++ src/SlmQueueDoctrine/Factory/DoctrineQueueFactory.php
@@ -11,33 +11,33 @@ use SlmQueueDoctrine\Queue\DoctrineQueue;
  */
 class DoctrineQueueFactory implements FactoryInterface
 {
-   /**
-    * {@inheritDoc}
-    */
-   public function createService(ServiceLocatorInterface $serviceLocator, $name = '', $requestedName = '')
-   {
-       $parentLocator = $serviceLocator->getServiceLocator();
+    /**
+     * {@inheritDoc}
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator, $name = '', $requestedName = '')
+    {
+        $parentLocator = $serviceLocator->getServiceLocator();
 
-       /** @var $doctrineOptions \SlmQueueDoctrine\Options\DoctrineOptions */
-       $doctrineOptions = $parentLocator->get('SlmQueueDoctrine\Options\DoctrineOptions');
+        /** @var $doctrineOptions \SlmQueueDoctrine\Options\DoctrineOptions */
+        $doctrineOptions = $parentLocator->get('SlmQueueDoctrine\Options\DoctrineOptions');
 
-       /** @var $connection \Doctrine\DBAL\Connection */
-       $connection       = $parentLocator->get($doctrineOptions->getConnection());
-       $tableName        = $doctrineOptions->getTableName();
-       $jobPluginManager = $parentLocator->get('SlmQueue\Job\JobPluginManager');
+        /** @var $connection \Doctrine\DBAL\Connection */
+        $connection       = $parentLocator->get($doctrineOptions->getConnection());
+        $tableName        = $doctrineOptions->getTableName();
+        $jobPluginManager = $parentLocator->get('SlmQueue\Job\JobPluginManager');
 
-       $table = new DoctrineQueue($connection, $tableName, $requestedName, $jobPluginManager);
+        $table = new DoctrineQueue($connection, $tableName, $requestedName, $jobPluginManager);
 
-       $config = $parentLocator->get('Config');
-       $options = array_key_exists($requestedName, $config['slm_queue']['queues']) ? $config['slm_queue']['queues'][$requestedName] : array();
+        $config = $parentLocator->get('Config');
+        $options = array_key_exists($requestedName, $config['slm_queue']['queues']) ? $config['slm_queue']['queues'][$requestedName] : array();
 
-       if (isset($options['sleep_when_idle'])) {
-           $table->setSleepWhenIdle($options['sleep_when_idle']);
-       }
-       
-       $table->setBuriedLifetime($doctrineOptions->getBuriedLifetime());
-       $table->setDeletedLifetime($doctrineOptions->getDeletedLifetime());
+        if (isset($options['sleep_when_idle'])) {
+            $table->setSleepWhenIdle($options['sleep_when_idle']);
+        }
 
-       return $table;
-   }
+        $table->setBuriedLifetime($doctrineOptions->getBuriedLifetime());
+        $table->setDeletedLifetime($doctrineOptions->getDeletedLifetime());
+
+        return $table;
+    }
 }
