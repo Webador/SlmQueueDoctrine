303e150673ebaad406b4baa994bd34dc9e5451f6
Bas Kamer <bas@bushbaby.nl>
2013-05-29 13:26:43 +0200

    corrects time() for timezone offset

--- tests/SlmQueueDoctrineTest/Queue/DoctrineTableTest.php
+++ tests/SlmQueueDoctrineTest/Queue/DoctrineTableTest.php
@@ -179,12 +179,17 @@ class DoctrineTableTest extends TestCase
         $job = new SimpleJob();
         $returnedCount = 0;
 
-        $this->tableQueue->push($job, array('scheduled' => time() + 10)); // must not be returned
-        $this->tableQueue->push($job, array('scheduled' => time() - 10));$returnedCount++;
-        $this->tableQueue->push($job, array('scheduled' => time() - 100));$returnedCount++;
+        $now = new DateTime(null, new DateTimeZone(date_default_timezone_get()));
+        $this->tableQueue->push($job, array('scheduled' =>  time() + $now->getOffset() + 10));
+        $this->assertNull($this->tableQueue->pop(), "Job is not due yet.");
+
+        $this->tableQueue->push($job, array('scheduled' => time() + $now->getOffset() + 10)); // must not be returned
+        $this->tableQueue->push($job, array('scheduled' => time() + $now->getOffset() - 10));$returnedCount++;
+        $this->tableQueue->push($job, array('scheduled' => time() + $now->getOffset() - 100));$returnedCount++;
+
         $firstJobId = $job->getId();
-        $this->tableQueue->push($job, array('scheduled' => time() - 50));$returnedCount++;
-        $this->tableQueue->push($job, array('scheduled' => time() - 30));$returnedCount++;
+        $this->tableQueue->push($job, array('scheduled' => time() + $now->getOffset()  - 50));$returnedCount++;
+        $this->tableQueue->push($job, array('scheduled' => time() + $now->getOffset()  - 30));$returnedCount++;
         $this->tableQueue->push($job, array('delay' => 100)); // must not be returned
         $this->tableQueue->push($job, array('delay' => -90)); $returnedCount++;
 
