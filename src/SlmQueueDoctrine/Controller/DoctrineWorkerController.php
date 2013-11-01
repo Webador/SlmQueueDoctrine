bb80e1b9a27495e5a4b7bdae34f68b75a39fd426
Bas Kamer <bas@bushbaby.nl>
2013-11-01 16:10:56 +0100

    POC implementation of maximum workers option

--- src/SlmQueueDoctrine/Controller/DoctrineWorkerController.php
+++ src/SlmQueueDoctrine/Controller/DoctrineWorkerController.php
@@ -2,6 +2,7 @@
 
 namespace SlmQueueDoctrine\Controller;
 
+use SlmQueue\Worker\WorkerEvent;
 use SlmQueueDoctrine\Queue\DoctrineQueueInterface;
 use SlmQueue\Controller\Exception\WorkerException;
 use SlmQueue\Controller\AbstractWorkerController;
@@ -12,6 +13,47 @@ use SlmQueue\Exception\ExceptionInterface;
  */
 class DoctrineWorkerController extends AbstractWorkerController
 {
+    /**
+     * @inheritdoc
+     */
+    public function processAction()
+    {
+        $options = $this->params()->fromRoute();
+
+        if (isset($options['max-workers']) && is_numeric($options['max-workers'])) {
+            $lockfile = sprintf('data/slm-queue-doctrine-worker-%s.cnt', $options['queue']);
+            if (file_exists($lockfile)) {
+                $cnt = (int) file_get_contents($lockfile);
+
+                if ($cnt >= (int) $options['max-workers']) {
+                    return sprintf(
+                        "Exceeding maximum workers '%s' this queue '%s'\n",
+                        $cnt,
+                        $options['queue']
+                    );
+                }
+            } else {
+                $cnt      = 0;
+            }
+
+            $this->getEventManager()->getSharedManager()->attach('SlmQueueDoctrine\Worker\DoctrineWorker', WorkerEvent::EVENT_PROCESS_QUEUE_PRE, function(WorkerEvent $e) use ($lockfile, $cnt) {
+                file_put_contents($lockfile, ++$cnt);
+            });
+
+            $this->getEventManager()->getSharedManager()->attach('SlmQueueDoctrine\Worker\DoctrineWorker', WorkerEvent::EVENT_PROCESS_QUEUE_POST, function(WorkerEvent $e) use ($lockfile, $cnt) {
+                if ($cnt >= 1) {
+                    file_put_contents($lockfile, $cnt);
+                } else {
+                    if (file_exists($lockfile)) {
+                        unlink($lockfile);
+                    }
+                }
+            });
+        }
+
+        return parent::processAction();
+    }
+
     /**
      * Recover long running jobs
      *
