2e74bd8a56ba15b95501770625fabf8e10a76606
Bas Kamer <bas@bushbaby.nl>
2014-10-09 10:01:21 +0200

    use reserved keywords

--- tests/SlmQueueDoctrineTest/Queue/DoctrineQueueTest.php
+++ tests/SlmQueueDoctrineTest/Queue/DoctrineQueueTest.php
@@ -66,6 +66,18 @@ class DoctrineQueueTest extends TestCase
         $this->assertEquals(1, $result['count']);
     }
 
+    public function testPushPop()
+    {
+        $this->markTestSkipped('Test skipped until SlmQueue:0.5.0 (see https://github.com/juriansluiman/SlmQueue/pull/111)');
+
+        $job = new SimpleJob();
+        $this->queue->push($job);
+
+        $poppedJob = $this->queue->pop();
+
+        $this->assertEquals($job, $poppedJob);
+    }
+
     public function testJobCanBePushedMoreThenOnce()
     {
         $job = new SimpleJob();
