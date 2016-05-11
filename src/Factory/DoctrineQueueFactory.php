3c1ad3579b1d375ad8a848722e7faffe547815e1
malinink <apmath@malinink.ru>
2016-05-11 10:06:18 +0300

    modify factories to be SM3 compatible

--- src/Factory/DoctrineQueueFactory.php
+++ src/Factory/DoctrineQueueFactory.php
@@ -7,6 +7,7 @@ use SlmQueueDoctrine\Options\DoctrineOptions;
 use SlmQueueDoctrine\Queue\DoctrineQueue;
 use Zend\ServiceManager\FactoryInterface;
 use Zend\ServiceManager\ServiceLocatorInterface;
+use Interop\Container\ContainerInterface;
 
 /**
  * DoctrineQueueFactory
@@ -16,21 +17,28 @@ class DoctrineQueueFactory implements FactoryInterface
     /**
      * {@inheritDoc}
      */
-    public function createService(ServiceLocatorInterface $serviceLocator, $name = '', $requestedName = '')
+    public function __invoke(ContainerInterface $container, $requestedName, array $options = null)
     {
-        $parentLocator = $serviceLocator->getServiceLocator();
-
-        $config        = $parentLocator->get('Config');
+        $config        = $container->get('config');
         $queuesOptions = $config['slm_queue']['queues'];
         $options       = isset($queuesOptions[$requestedName]) ? $queuesOptions[$requestedName] : [];
         $queueOptions  = new DoctrineOptions($options);
 
         /** @var $connection \Doctrine\DBAL\Connection */
-        $connection       = $parentLocator->get($queueOptions->getConnection());
-        $jobPluginManager = $parentLocator->get(JobPluginManager::class);
+        $connection       = $container->get($queueOptions->getConnection());
+        $jobPluginManager = $container->get(JobPluginManager::class);
 
         $queue = new DoctrineQueue($connection, $queueOptions, $requestedName, $jobPluginManager);
 
         return $queue;
     }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator, $name = '', $requestedName = '')
+    {
+        return $this($serviceLocator->getServiceLocator(), $requestedName);
+    }
 }
+
