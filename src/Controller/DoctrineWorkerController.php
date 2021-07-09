5646c9055d54b4c2138be6b47eee093aca185d66
Roel van Duijnhoven <roel@vanduijnhoven.com>
2021-07-09 14:04:15 +0200

    Inline code from SlmQueue.

--- src/Controller/DoctrineWorkerController.php
+++ src/Controller/DoctrineWorkerController.php
@@ -2,17 +2,58 @@
 
 namespace SlmQueueDoctrine\Controller;
 
-use SlmQueue\Controller\AbstractWorkerController;
+use Laminas\Mvc\Controller\AbstractActionController;
 use SlmQueue\Controller\Exception\WorkerProcessException;
 use SlmQueue\Exception\ExceptionInterface;
 use SlmQueue\Queue\QueuePluginManager;
+use SlmQueue\Worker\WorkerInterface;
 use SlmQueueDoctrine\Queue\DoctrineQueueInterface;
 
 /**
  * Worker controller
  */
-class DoctrineWorkerController extends AbstractWorkerController
+class DoctrineWorkerController extends AbstractActionController
 {
+
+    /**
+     * @var WorkerInterface
+     */
+    protected $worker;
+
+    /**
+     * @var QueuePluginManager
+     */
+    protected $queuePluginManager;
+
+    /**
+     * @param WorkerInterface    $worker
+     * @param QueuePluginManager $queuePluginManager
+     */
+    public function __construct(WorkerInterface $worker, QueuePluginManager $queuePluginManager)
+    {
+        $this->worker = $worker;
+        $this->queuePluginManager = $queuePluginManager;
+    }
+
+    public function processAction(): string
+    {
+        $options = $this->params()->fromRoute();
+        $name = $options['queue'];
+        $queue = $this->queuePluginManager->get($name);
+
+        try {
+            $messages = $this->worker->processQueue($queue, $options);
+        } catch (ExceptionInterface $e) {
+            throw new WorkerProcessException(
+                'Caught exception while processing queue',
+                $e->getCode(),
+                $e
+            );
+        }
+
+        return $this->formatOutput($name, $messages);
+    }
+
     /**
      * Recover long running jobs
      */
@@ -36,6 +77,19 @@ class DoctrineWorkerController extends AbstractWorkerController
             "\nWork for queue %s is done, %s jobs were recovered\n\n",
             $queueName,
             $count
+        );
+    }
+
+    protected function formatOutput(string $queueName, array $messages = []): string
+    {
+        $messages = implode("\n", array_map(function (string $message): string {
+            return sprintf(' - %s', $message);
+        }, $messages));
+
+        return sprintf(
+            "Finished worker for queue '%s':\n%s\n",
+            $queueName,
+            $messages
         );
     }
 }
