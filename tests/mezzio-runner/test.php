a503acf7747e14fcf5f6c6c665f6fe086e425b5e
Koen Kivits <koen@kivits.com>
2021-10-15 10:42:01 +0200

    Clean up mezzio-runner

--- tests/mezzio-runner/test.php
+++ tests/mezzio-runner/test.php
@@ -9,7 +9,7 @@ require 'vendor/autoload.php';
 $serviceManager = require 'config/container.php';
 $entityManager = $serviceManager->get('doctrine.entitymanager.orm_default');
 $connection = $entityManager->getConnection();
-$connection->executeQuery(file_get_contents(__DIR__ . '/../../queue_default.sqlite'));
+$connection->executeQuery(file_get_contents(__DIR__ . '/../queue_default.sqlite'));
 
 // Populate with a job
 $serviceManager->get(\SlmQueue\Queue\QueuePluginManager::class)->get('default')->push(new \App\TestJob());
@@ -18,10 +18,10 @@ $serviceManager->get(\SlmQueue\Queue\QueuePluginManager::class)->get('default')-
 exec('vendor/bin/laminas slm-queue-doctrine:start default');
 
 // Assert that file was generated?
-if (@file_get_contents('temp/succesfull') !== 'YES') {
-    echo 'Test was NOT successfull.';
+if (@file_get_contents('temp/succesful') !== 'YES') {
+    echo 'Test was NOT successful.';
     exit(1);
 }
 
-echo 'Test was successfull.';
+echo 'Test was successful.';
 exit(0);
